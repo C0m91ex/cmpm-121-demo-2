f91d3da68e81588c755509d514a983a4046dd277
C0m91ex <jtorr124@ucsc.edu>
2024-10-22 12:45:40 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import "./style.css";
 
-const APP_NAME = "Sketch Pad";
+const APP_NAME = "Etcha A Sketch Pad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+// Set the title of the webpage
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+// Create and append the app title (h1)
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+app.appendChild(title);
+
+// Create and append the canvas
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "sketchCanvas";  
+app.appendChild(canvas);
